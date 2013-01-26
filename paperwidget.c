e993818dafe62b37021cd15cece8105d843bfdad
Robert Lowe <pngwen@acm.org>
2013-01-26 17:47:23 -0500

    - Added standard types and fields for the dropdowns. - Converted combo boxes to combo text boxes to simplify things a bit

--- paperwidget.c
+++ paperwidget.c
@@ -17,18 +17,39 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include "paperwidget.h"
 
 static void addLabelRow(gra_paper_widget *pw, const gchar* text, GtkWidget *widget);
 static void addFieldRow(gra_paper_widget *pw);
-static void addRow(gra_paper_widget *pw, GtkWidget *w1, GtkWidget *w2);
+static void addRow(gra_paper_widget *pw, GtkWidget *row, GtkWidget *w1, GtkWidget *w2);
 static void addFieldButtonClicked(GtkWidget *widget, gpointer data);
+
+/**
+ * These are the standard bibtex fields.  Minus the ones which are
+ * always defined.
+ */
+static gchar* standardFields[] = {
+  "address", "editor", "eprint", "institution", "journal", "key",
+    "month", "note", "organization", "pages", "publisher", "school",
+    "series", "ur", "volume"
+};
+
+
+/**
+ * These are the standard document types used in bibtex.
+ */
+static gchar* standardTypes[] = {
+  "Article", "Book", "Booklet", "Conference", "Inbook", "Incollection",
+    "Inproceedings", "Manual", "Mastersthesis", "misc", "Phdthesis",
+    "Techreport", "Unpublished"
+};
+
 
 gra_paper_widget *
 gra_paper_widget_new(gra_paper_t *paper) {
   gra_paper_widget *pw;
   GtkWidget *button;
+  int i, n;
   
   
   /* TODO: Magic to glue to the paper struct */
@@ -37,7 +58,7 @@ gra_paper_widget_new(gra_paper_t *paper) {
   pw = g_malloc(sizeof(gra_paper_widget));
   pw->widget = gtk_vbox_new(FALSE, 1);
   pw->fieldBox = gtk_vbox_new(TRUE, 2);
-  pw->type = gtk_combo_box_new_with_entry();
+  pw->type = gtk_combo_box_text_new_with_entry();
   pw->title = gtk_entry_new();
   pw->author = gtk_entry_new();
   pw->year = gtk_entry_new();
@@ -51,7 +72,18 @@ gra_paper_widget_new(gra_paper_t *paper) {
   addLabelRow(pw, "Title", pw->title);
   addLabelRow(pw, "Author", pw->author);
   addLabelRow(pw, "Year", pw->year);
+  addRow(pw,
+         gtk_hbox_new(TRUE, 2),
+         gtk_label_new("Field"),
+         gtk_label_new("Value"));
   addFieldRow(pw);
+
+  /* populate type dropdown */
+  n = sizeof(standardTypes) / sizeof(standardTypes[0]);
+  for(i=0; i<n; i++) {
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(pw->type),
+                                   standardTypes[i]);
+  }
 
   /*add the add button */
   button = gtk_button_new_with_label("Add Field");
@@ -76,32 +108,35 @@ gra_paper_widget_new(gra_paper_t *paper) {
 static void
 addLabelRow(gra_paper_widget *pw, const gchar* text, GtkWidget *widget) {
   GtkWidget *label;
-
+  
   /* create the label, then then the field row */
   label = gtk_label_new(text);
   gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
-  addRow(pw, label, widget);
+
+  addRow(pw, gtk_hbox_new(FALSE, 2), label, widget);
 }
 
 
 static void
 addFieldRow(gra_paper_widget *pw) {
   GtkWidget *field, *value;
-
-  /* create the combo boxes */
-  field = gtk_combo_box_new_with_entry();
+  int i;
+  int n = sizeof(standardFields) / sizeof(standardFields[0]);
+  
+  /* create the combo box and entry */
+  field = gtk_combo_box_text_new_with_entry();
   value = gtk_entry_new();
 
-  addRow(pw, field, value);
+  for(i=0; i<n; i++) {
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(field), standardFields[i]);
+  }
+
+  addRow(pw, gtk_hbox_new(TRUE, 2), field, value);
 }
 
 
 static void
-addRow(gra_paper_widget *pw, GtkWidget *w1, GtkWidget *w2) {
-  GtkWidget *row;
-
-  /* allocate this row */
-  row = gtk_hbox_new(FALSE, 2);
+addRow(gra_paper_widget *pw, GtkWidget *row, GtkWidget *w1, GtkWidget *w2) {
   gtk_widget_set_size_request(w1, 80, -1);
   gtk_box_pack_start(GTK_BOX(row), w1, FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(row), w2, TRUE, TRUE, 0);
@@ -109,7 +144,6 @@ addRow(gra_paper_widget *pw, GtkWidget *w1, GtkWidget *w2) {
   gtk_widget_show(w1);
   gtk_widget_show(w2);
   gtk_widget_show(row);
-
 }
 
 
